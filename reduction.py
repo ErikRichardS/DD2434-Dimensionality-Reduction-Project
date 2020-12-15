f4f60e8bf4b832b9f0b4bfb8968380d8f1e6b425
Erik <erik.r.svensson@telia.com>
2020-12-15 15:37:18 +0100

    Reduction functions skeleton

---
+++ reduction.py
@@ -0,0 +1,32 @@
+import torch
+
+import numpy as np
+
+
+
+
+# Principal Component Analysis
+def PCA(input_matrix):
+
+	return input_matrix
+
+
+
+def RandomProjection(input_matrix):
+
+	return input_matrix
+
+
+
+def StochasticRandomProjection(input_matrix):
+
+	return input_matrix
+
+
+
+def DCT(input_matrix):
+
+	return input_matrix
+
+
+
