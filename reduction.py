0a5a0ffb3cac90053db6eba5e2f87820feae7738
evegaard <64893461+evegaard@users.noreply.github.com>
2020-12-22 15:22:55 +0100

    updated RP function

--- reduction.py
+++ reduction.py
@@ -13,8 +13,15 @@ def PCA(input_matrix):
 
 
 def RandomProjection(input_matrix):
-
-	return input_matrix
+	# create a matrix with random normal (Gaussian) distributed elements
+	d = len(X)
+	mean, standard_deviation = 0, 1 
+	R = np.random.normal(mean, standard_deviation, (k, d))
+	# normalize all column vectors in R to unit vectors
+	R_normalized = R/np.linalg.norm(R, ord=2, axis=0, keepdims=True)
+	
+	RP = np.matmul(R_normalized, X)
+	return RP
 
 
 
