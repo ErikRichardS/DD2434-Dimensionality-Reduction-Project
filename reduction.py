aec12cd78d3004cd6f418ea14ddea6946add3a5e
evegaard <64893461+evegaard@users.noreply.github.com>
2020-12-28 12:07:25 +0100

    upd rp, epsilon and k

--- reduction.py
+++ reduction.py
@@ -1,9 +1,17 @@
 import torch
-
 import numpy as np
-
 import random
 
+# parameter that controls the quality of distance preservation
+epsilon = 0.01
+
+# k-value
+# k = K_rp(epsilon, n) ?
+# k = K_srp(epsilon, n) ?
+
+def K_rp(epsilon, n):
+  k = 4*np.log(n)/(epsilon^2/2 + epsilon^3/3)
+  return k
 
 def center_matrix(matrix):
 	n = matrix.shape[1]
@@ -12,6 +20,7 @@ def center_matrix(matrix):
 
 	return matrix
 
+# Singular Value Decomposition
 def svd(matrix):
 	return np.linalg.svd(matrix)
 
@@ -26,29 +35,28 @@ def PCA(X, k=2):
 
 	return pca_matrix
 
-
-# input: d-dimensional matrix, k 
-# output: k-dimensional matrix (k << d)
-def RandomProjection(X, k=2):
+# Random Projection
+def RandomProjection(matrix, k=2):
 	# create a matrix with random normal (Gaussian) distributed elements
-	d = len(X)
+	d = len(matrix)
 	mean, standard_deviation = 0, 1 
-	R = np.random.normal(mean, standard_deviation, (k, d))
+	r = np.random.normal(mean, standard_deviation, (k, d))
 	# normalize all column vectors in R to unit vectors
-	R_normalized = R/np.linalg.norm(R, ord=2, axis=0, keepdims=True)
+	r_unitnorm = R/np.linalg.norm(R, ord=2, axis=0, keepdims=True)
 	
-	RP = R_normalized @ X
-	return RP
+	rp_matrix = r_unitnorm @ matrix
+	
+	return rp_matrix
 
 
-
+# Sparse Random Projection
 def SparseRandomProjection(matrix, k=2):
 	d, _ = matrix.shape
 	srp = np.zeros((k, d))
 
-	# 1	  with 1/6 propability
-	# 0	  with 4/6 propability
-	# -1  with 1/6 propability
+	# 1	with 1/6 propability
+	# 0	with 4/6 propability
+	# -1	with 1/6 propability
 	rand_val = [1, 0, 0, 0, 0, -1]
 
 	for i in range(k):
@@ -60,7 +68,7 @@ def SparseRandomProjection(matrix, k=2):
 	return srp_matrix
 
 
-
+# Discrete Cosine Transform 
 def DCT(input_matrix, k=2):
 
 	return input_matrix
