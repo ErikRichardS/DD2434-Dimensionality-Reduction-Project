f99f9662da71742e0e2e00f7b9f34b096171bf9f
evegaard <64893461+evegaard@users.noreply.github.com>
2020-12-29 12:40:23 +0100

    updated RP, check_property, k_rp, k_srp

--- reduction.py
+++ reduction.py
@@ -3,18 +3,26 @@ import numpy as np
 import random
 
 # parameter that controls the quality of distance preservation
-epsilon = 0.01
-# number of dimensions in input matrix
-# d = len(matrix)
-# number of samples in input matrix
-# n = len(matrix[0])
+epsilon = 0.5
+
+# beta-value
+# beta = 0.1
 
 # k-value
-# k = K_rp(epsilon, n) ?
-# k = K_srp(epsilon, n) ?
+# k = k_rp(epsilon, n) ?
+# k = k_srp(epsilon, n, beta) ?
 
-def K_rp(epsilon, n):
-  k = 4*np.log(n)/(epsilon**2/2 + epsilon**3/3)
+# number of dimensions in input matrix
+# d = matrix.shape[0]
+# number of samples in input matrix
+# n = matrix.shape[1]
+
+def k_rp(epsilon, n):
+  k = 4*np.log(n)/(epsilon**2/2 - epsilon**3/3)
+  return k
+
+def k_srp(epsilon, n, beta):
+  k = (4 + 2*beta)*np.log(n)/(epsilon**2/2 - epsilon**3/3)
   return k
 
 def center_matrix(matrix):
@@ -29,8 +37,8 @@ def svd(matrix):
 	return np.linalg.svd(matrix)
 
 # Principal Component Analysis
-def PCA(X, k=2):
-	mat = center_matrix(X)
+def PCA(matrix, k=2):
+	mat = center_matrix(matrix)
 
 	u, s, vh = svd(mat)
 	uk = np.transpose(u[:,:k] )
@@ -41,21 +49,43 @@ def PCA(X, k=2):
 
 # Random Projection
 def RandomProjection(matrix, k=2):
-	# create a matrix with random normal (Gaussian) distributed elements
-	d = len(matrix)
-	mean, standard_deviation = 0, 1 
-	r = np.random.normal(mean, standard_deviation, (k, d))
-	# normalize all column vectors in R to unit vectors
-	r_unitnorm = R/np.linalg.norm(R, ord=2, axis=0, keepdims=True)
-	
-	rp_matrix = r_unitnorm @ matrix
-	
-	return rp_matrix
+	d, n = matrix.shape
+	for i in range(0,n):
+		# create a matrix with random normal (Gaussian) distributed elements
+	    	mean, standard_deviation = 0, 1 
+    		r = np.random.normal(mean, standard_deviation, (k, d))
+    		# normalize all column vectors in r to unit vectors
+    		r_unitnorm = r/np.linalg.norm(r, ord=2, axis=0, keepdims=True)
+
+    		# compute the lower-dimensional random projection matrix
+    		rp_matrix = r_unitnorm @ matrix
+
+		# check if it satisfies the desired JL property
+		result = check_property(matrix, rp_matrix, n, d, k)
+		# if so, return random projection matrix
+		if result == True:
+			return rp_matrix
+
+def check_property(matrix, rp_matrix, n, d, k):
+  	for i in range(0,n):
+    		for j in range(0,n):
+      			xi = matrix[:,i]
+      			xj = matrix[:,j]
+      			dist_x = np.linalg.norm(xi-xj)
+			
+      			f_xi = rp_matrix[:,i]
+      			f_xj = rp_matrix[:,j]
+      			dist_fx = np.linalg.norm(f_xi-f_xj)
+			#dist_fx = np.sqrt(d/k)*np.linalg.norm(f_xi-f_xj)
+
+      			if (abs(dist_x-dist_fx)/dist_x > epsilon):
+        			return False
+  	return True
 
 
 # Sparse Random Projection
 def SparseRandomProjection(matrix, k=2):
-	d, _ = matrix.shape
+	d = matrix.shape[0]
 	srp = np.zeros((k, d))
 
 	# 1	with 1/6 propability
