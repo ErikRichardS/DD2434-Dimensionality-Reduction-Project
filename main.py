526dc8d1958eca7e0b57e19dc57fd5867f368707
MaxSvjatoha <maxsvjatoha@gmail.com>
2021-01-13 01:23:26 +0100

    First attempt at integrating dct with parser. Getting dimension errors when using one row at a time.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 from reduction import *
 from Parser import *
@@ -31,15 +32,33 @@ def test_sparse_random_projection(matrix, dims):
 	return 0
 
 def test_dct(matrix, dims):
-	return 0
-
-
-
-matrix = readTextFiles()
+    results_max = np.zeros(len(dims))
+    results_avr = np.zeros(len(dims))
+    results_min = np.zeros(len(dims))
+    
+    d, N = matrix.shape # (209404, 19997) for article dataset
+    
+    # Assume columns = data points, rows = dims in matrix
+    for i, k in enumerate(dims):
+        dct_output = np.empty((d, k))
+        for datapoint in matrix.transpose():
+            # TypeError: sparse matrix length is ambiguous; use getnnz() or shape[0]
+            dct_row = DCT(datapoint[0], k)  
+            dct_output.append(dct_row)
+        dct_output = dct_output.transpose() # Transpose back to parser format
+        error = check_quality(matrix, dct_output, N, d, k, scaling=False)
+        
+        results_avr[i] = error
+    
+    return results_avr
 
 indx = [2**i for i in range(1, 11)]
 
-res = test_pca(matrix, indx)
+matrix = readTextFiles()
 
-print(res[0])
-print(res[1])
+res_dct = test_dct(matrix, indx)
+
+# res = test_pca(matrix, indx)
+
+# print(res[0])
+# print(res[1])
